3e37f462bd446f220d2d9eaaa89789eff93d9d54
Gustavo Rangel <ggrangel@amazon.com>
2022-11-10 10:05:44 -0300

    change vimwiki folder

--- nvim/lua/plugins/vimwiki.lua
+++ nvim/lua/plugins/vimwiki.lua
@@ -1,9 +1,9 @@
-vim.cmd [[
-let g:vimwiki_list = [{ 'path': '~/drive/GAGA/vimwiki', 'syntax': 'markdown', 'ext': '.md' }]
+vim.cmd([[
+let g:vimwiki_list = [{ 'path': '~/Insync/gustavorangel91@gmail.com/Google Drive/GAGA/vimwiki/', 'syntax': 'markdown', 'ext': '.md' }]
 let g:vimwiki_ext2syntax = { '.md': 'markdown', '.markdown': 'markdown', '.mdown': 'markdown' }
 
 " Makes vimwiki markdown links as [text](text.md) instead of [text](text)
 let g:vimwiki_markdown_link_ext = 1
 
 let g:markdown_folding = 1
-]]
+]])
